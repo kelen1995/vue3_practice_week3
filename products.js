64e0512ab82e9e40027b9ce2316cfff47049b82a
kelen1995 <kelen@gmail.com>
2022-02-12 21:30:19 +0800

    feat: 完成編輯產品 & 完成 token 檢查功能

--- products.js
+++ products.js
@@ -9,8 +9,9 @@ const app = createApp({
         return {
             products: [],
             tempProduct: {
-                imagesUrl:[],
+                imagesUrl:[]
             },
+            isNew: false,
         }
     },
     methods: {
@@ -23,19 +24,38 @@ const app = createApp({
                 console.log(err.response);
             })
         },
-        showProductModal() {
+        showProductModal(status, product) {
+            switch (status) {
+                case 'add':
+                    this.tempProduct = {imagesUrl:[]};
+                    this.isNew = true;
+                    break;
+                case 'edit':
+                    this.tempProduct = {...product};
+                    this.isNew = false;
+                    break;                    
+            }
             productModal.show();
         },
         hideProductModal() {
             productModal.hide();
         },
-        createProduct() {
-            axios.post(`${apiUrl}/api/${apiPath}/admin/product`, {
+        updateProduct() {
+            let method = 'post';
+            let url = `${apiUrl}/api/${apiPath}/admin/product`;
+            let message = '產品新增完成';
+
+            if (!this.isNew) {
+                method = 'put';
+                url = `${apiUrl}/api/${apiPath}/admin/product/${this.tempProduct.id}`;
+                message = '產品修改完成';
+            }
+
+            axios[method](url, {
                 "data": this.tempProduct
             })
             .then(res => {
-                console.log(res.data);
-                alert('產品新增完成');
+                alert(message);
                 this.hideProductModal();
                 this.getProducts();
                 this.tempProduct = {imagesUrl:[]};
@@ -44,13 +64,23 @@ const app = createApp({
                 console.log(err);
                 alert('產品新增失敗');
             })
+        },
+        checkUser() {// 檢查是否有登入 token
+            axios.defaults.headers.common.Authorization = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+            axios.post(`${apiUrl}/api/user/check`)
+            .then(res => {
+                productModal = new bootstrap.Modal(document.getElementById('productModal'), {});
+                this.getProducts();
+            })
+            .catch(err => {
+                console.log(err);
+                window.location = 'index.html'
+            })
         }
 
     },
     mounted() {
-        axios.defaults.headers.common.Authorization = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-        productModal = new bootstrap.Modal(document.getElementById('productModal'), {});
-        this.getProducts();
+        this.checkUser();
     }
 });
 
